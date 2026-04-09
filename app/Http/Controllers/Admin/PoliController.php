59b777467f50e5cbaadca1fe77390de169d1dd71
kmilawn <najwahk354@gmail.com>
2026-04-09 19:39:24 +0700

    Tugas:2

---
+++ app/Http/Controllers/Admin/PoliController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Poli;
+use Illuminate\Http\Request;
+
+class PoliController extends Controller
+{
+    public function index()
+    {
+        $polis = Poli::all();
+        return view('admin.polis.index', compact('polis'));
+    }
+
+    public function create()
+    {
+        return view('admin.polis.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'nama_poli' => 'required',
+            'keterangan' => 'nullable',
+        ]);
+
+        Poli::create($validated);
+        return redirect()->route('polis.index')->with('success', 'Poli berhasil ditambahkan')->with('type', 'success');
+    }
+
+    public function show(string $id)
+    {
+        //
+    }
+
+    public function edit(string $id)
+    {
+        $poli = Poli::findOrFail($id);
+        return view('admin.polis.edit', compact('poli')); 
+    }
+
+    public function update(Request $request, string $id)
+    {
+        $validated = $request->validate([
+            'nama_poli' => 'required',
+            'keterangan' => 'nullable',
+        ]);
+
+        $poli = Poli::findOrFail($id);
+        $poli->update($validated);
+        return redirect()->route('polis.index')->with('success', 'Poli berhasil di update');
+    }
+
+    public function destroy(string $id)
+    {
+        $poli = Poli::findOrFail($id);
+        $poli->delete($poli);
+        return redirect()->route('polis.index')->with('success', 'Polis Berhasil di hapus!');
+    }
+}
